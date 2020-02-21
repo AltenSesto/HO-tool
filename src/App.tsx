332fafdb9f8d1485b7b016504cf891220058b389
Dzmitry Varfalameyeu <dvarfalamey@SESTO-L1903002>
2020-02-21 16:06:27 +0100

    Primitive error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SystemDescription from './components/system-description/system-description';
+import ErrorBoundary from './components/error-boundary';
 
 const App: React.FC = () => {
   // Routing goes here
 
   return (
-    <SystemDescription></SystemDescription>
+    <ErrorBoundary>
+      <SystemDescription></SystemDescription>
+    </ErrorBoundary>
   );
 }
 
