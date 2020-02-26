cb8254dacecf7fa41b3bebea2ebf3b73a4a14cc1
Dzmitry Varfalameyeu <dvarfalamey@SESTO-L1903002>
2020-02-26 12:01:57 +0100

    Fixed action button position on nodes manipulation

--- src/components/graph/subsystem-actions.tsx
+++ src/components/graph/subsystem-actions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Core, Singular, NodeSingular, EventObjectNode } from 'cytoscape';
+import { Core, Singular, NodeSingular, EventObjectNode, EventObject } from 'cytoscape';
 import Subsystem from '../../entities/system-description/subsystem';
 import ElementActions from './element-actions';
 
@@ -14,13 +14,22 @@ interface Props {
 export default class SubsystemActions extends React.Component<Props> {
 
     private readonly EVENT_DRAGFREE = 'dragfree';
+    private readonly EVENT_POSITION = 'position';
+    private readonly EVENT_MOVE = 'move';
+    private readonly EVENT_ADD = 'add';
+    private readonly EVENT_REMOVE = 'remove';
 
     private ele: NodeSingular | null = null;
+    private children: string[] = [];
 
     constructor(props: Props) {
         super(props);
         this.deleteWithChildren = this.deleteWithChildren.bind(this);
         this.initPopper = this.initPopper.bind(this);
+        this.updatePositionOnMove = this.updatePositionOnMove.bind(this);
+        this.updatePositionOnChildAdded = this.updatePositionOnChildAdded.bind(this);
+        this.updatePositionOnChildRemoved = this.updatePositionOnChildRemoved.bind(this);
+        this.updatePositionOnParentChanged = this.updatePositionOnParentChanged.bind(this);
         this.savePosition = this.savePosition.bind(this);
     }
 
@@ -40,7 +49,10 @@ export default class SubsystemActions extends React.Component<Props> {
     }
 
     componentWillUnmount() {
-        this.ele && this.ele.off(this.EVENT_DRAGFREE, undefined, this.savePosition);
+        this.ele && this.ele.off(this.EVENT_DRAGFREE, undefined, this.updatePositionOnMove);
+        this.props.cy.off(this.EVENT_ADD, undefined, this.updatePositionOnChildAdded);
+        this.props.cy.off(this.EVENT_REMOVE, undefined, this.updatePositionOnChildRemoved);
+        this.props.cy.off(this.EVENT_MOVE, undefined, this.updatePositionOnParentChanged);
     }
     
     private deleteWithChildren() {
@@ -57,16 +69,59 @@ export default class SubsystemActions extends React.Component<Props> {
         this.props.subsystemDeleted(ids);
     }
 
-    private savePosition(event: EventObjectNode) {
+    private updatePositionOnMove(event: EventObjectNode) {
         const newPosition = event.target.position();
-        const updatedObj = { ...this.props.subsystem, ...{ posX: newPosition.x, posY: newPosition.y } };
+        if (this.ele) {
+            this.ele.children().forEach(e => { e.trigger(this.EVENT_POSITION); });
+        }
+        this.savePosition(newPosition);
+    }
+
+    private updatePositionOnChildAdded(event: EventObject) {
+        const node: Singular = event.target.element();
+        if (node.isNode() && this.ele) {
+            const parent = node.parent();
+            if (parent.length > 0 && parent[0].data().id === this.props.subsystem.id) {
+                this.children = this.children.concat(node.data().id);
+                this.ele.trigger(this.EVENT_POSITION);
+            }
+        }
+    }
+
+    private updatePositionOnChildRemoved(event: EventObject) {
+        const node: Singular = event.target.element();
+        if (node.isNode() && this.ele) {
+            const nodeId = node.data().id;
+            if (this.children.indexOf(nodeId) > -1) {
+                this.children = this.children.filter(e => e !== nodeId);
+                this.ele.trigger(this.EVENT_POSITION);
+                // if no more children we have to track our position ourself
+                this.savePosition(this.ele.position());
+            }
+        }
+    }
+
+    private updatePositionOnParentChanged(event: EventObject) {
+        this.updatePositionOnChildAdded(event);
+        this.updatePositionOnChildRemoved(event);
+    }
+
+    private savePosition(position: cytoscape.Position) {
+        if (this.ele && this.ele.children().length > 0) {
+            // position of non-empty subsystem is defined by its children
+            return;
+        }
+        const updatedObj = { ...this.props.subsystem, ...{ posX: position.x, posY: position.y } };
         this.props.subsystemRepositioned(updatedObj);
     }    
 
-    private initPopper(popperObj: any, ele: Singular) {
+    private initPopper(_popperObj: any, ele: Singular) {
         if (ele.isNode()) {
             this.ele = ele;
-            ele.on(this.EVENT_DRAGFREE, this.savePosition);
+            ele.on(this.EVENT_DRAGFREE, this.updatePositionOnMove);
+            this.props.cy.on(this.EVENT_REMOVE, this.updatePositionOnChildRemoved);
+            this.props.cy.on(this.EVENT_ADD, this.updatePositionOnChildAdded);
+            this.props.cy.on(this.EVENT_MOVE, this.updatePositionOnParentChanged);
         }
     }
 };
